2adf6094c8e55b6af744c826a6fa29ae433f73ab
alexbol99 <alexbol99@gmail.com>
2015-11-22 23:42:14 +0200

    Experiments with upload file to Parse Cloud, save in object and retrieve

--- source/main.js
+++ source/main.js
@@ -5,10 +5,9 @@
 import { Parse } from '../lib/parse-1.6.7.min';
 import { App } from 'app';
 
-Parse.initialize("MqfgDGIMgptBIgS6NqUMydGmjlXsfZaviORg4g2B","6x0jRJz3pUX4By1hzgonTMBPsCgSlpNE7kRNKxxc");
-
+/*
 ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
-
+*/
